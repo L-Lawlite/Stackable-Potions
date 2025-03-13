303a22ca1c6de580f02a94df529e467adca5c22e
Lawliet <junirobert21@gmail.com>
2025-03-13 15:47:19 +0530

    Completed the whole mod

---
+++ src/main/java/net/lawliet/stackable_potion/Config.java
@@ -0,0 +1,49 @@
+package net.lawliet.stackable_potion;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Neo's config APIs
+@EventBusSubscriber(modid = StackablePotionMod.MODID, bus = EventBusSubscriber.Bus.MOD)
+public class Config
+{
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    private static final ModConfigSpec.IntValue POTION_STACK_NUMBER = BUILDER
+            .comment("Stack Number for Drinkable Potion")
+            .defineInRange("potionStackNumber", 3, 0, 64);
+
+    private static final ModConfigSpec.IntValue SPLASH_POTION_STACK_NUMBER = BUILDER
+            .comment("Stack Number for Splash Potion")
+            .defineInRange("splashPotionStackNumber", 3, 0, 64);
+
+    private static final ModConfigSpec.IntValue LINGERING_POTION_STACK_NUMBER = BUILDER
+            .comment("Stack Number for Lingering Potion")
+            .defineInRange("lingeringPotionStackNumber", 3, 0, 64);
+
+
+    static final ModConfigSpec SPEC = BUILDER.build();
+
+    public static int potionStackNumber,splashPotionStackNumber,LingeringPotionStackNumber;
+
+
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+        potionStackNumber = POTION_STACK_NUMBER.get();
+        splashPotionStackNumber = SPLASH_POTION_STACK_NUMBER.get();
+        LingeringPotionStackNumber = LINGERING_POTION_STACK_NUMBER.get();
+
+    }
+}
---
+++ src/main/java/net/lawliet/stackable_potion/events/ModifyDefaultComponents.java
@@ -0,0 +1,19 @@
+package net.lawliet.stackable_potion.events;
+
+import net.lawliet.stackable_potion.Config;
+import net.lawliet.stackable_potion.StackablePotionMod;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.Items;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.ModifyDefaultComponentsEvent;
+
+@EventBusSubscriber(modid = StackablePotionMod.MODID,bus = EventBusSubscriber.Bus.MOD)
+public class ModifyDefaultComponents {
+    @SubscribeEvent
+    public static void modifyItem(ModifyDefaultComponentsEvent event) {
+        event.modify(Items.POTION, b -> b.set(DataComponents.MAX_STACK_SIZE, Config.potionStackNumber));
+        event.modify(Items.SPLASH_POTION, b -> b.set(DataComponents.MAX_STACK_SIZE,Config.splashPotionStackNumber));
+        event.modify(Items.LINGERING_POTION, b -> b.set(DataComponents.MAX_STACK_SIZE,Config.LingeringPotionStackNumber));
+    }
+}
