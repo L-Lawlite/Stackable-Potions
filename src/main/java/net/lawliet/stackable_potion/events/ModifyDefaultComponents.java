599b0fce3c525fb5e073b0879179fa56a33b3cbe
Lawliet <junirobert21@gmail.com>
2025-10-06 19:00:04 +0530

    1.21.7 with crash

--- src/main/java/net/lawliet/stackable_potion/events/ModifyDefaultComponents.java
+++ src/main/java/net/lawliet/stackable_potion/events/ModifyDefaultComponents.java
@@ -8,7 +8,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.ModifyDefaultComponentsEvent;
 
-@EventBusSubscriber(modid = StackablePotionMod.MODID,bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = StackablePotionMod.MODID)
 public class ModifyDefaultComponents {
     @SubscribeEvent
     public static void modifyItem(ModifyDefaultComponentsEvent event) {
